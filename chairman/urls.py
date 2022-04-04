76c717080819df267505f3a2545f32d35df96ec9
Shohag-Rana <69742923+Shohag-Rana@users.noreply.github.com>
2022-04-04 22:22:43 +0600

    teacher student delete

--- chairman/urls.py
+++ chairman/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
    	path('chairman_pswreset2/', views.chairman_password_reset2, name= 'chairman_pswreset2'),
    	path('teacher_list/', views.teacher_list, name= 'teacher_list'),
    	path('add_courses/', views.add_courses, name= 'add_courses'),
-
+   	path('chairman_teacher_reg/', views.chairman_teacher_reg, name= 'chairman_teacher_reg'),
+   	path('chairman_student_reg/', views.chairman_student_reg, name= 'chairman_student_reg'),
+   	path('delete_faculty_page/', views.delete_faculty_page, name= 'delete_faculty_page'),
+   	path('delete_student_page/', views.delete_student_page, name= 'delete_student_page'),
+   	path('delete_current_faculty/<int:faculty_id>/', views.delete_current_faculty, name= 'delete_current_faculty'),
+   	path('delete_current_student/<int:student_id>/', views.delete_current_student, name= 'delete_current_student'),
 ]
--- chairman/views.py
+++ chairman/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from authentication.models import *
 from chairman.forms import CourseForm
 from chairman.models import Course
+from authentication.forms import StudentRegForm, TeacherRegForm
 
 # Create your views here.
 def chairman_profile(request):
@@ -77,3 +78,71 @@ def add_courses(request):
             return HttpResponseRedirect('/chairman/add_courses/')
     form = CourseForm()
     return render(request, 'chairman/add_courses.html', {'form': form, 'courses': allCourses})
+
+def chairman_teacher_reg(request):
+    if request.user.is_admin:
+        if request.method == 'POST':
+            form = TeacherRegForm(request.POST, request.FILES)
+            if form.is_valid():
+                mail = form.cleaned_data['email']
+                flag = False
+                for m in Teacher_email.objects.all():
+                    if m.email == mail:
+                        flag = True
+                        break
+                if flag == True:
+                    messages.success(request, 'Successfully create a Faculty Member')
+                    messages.success(request, 'Activate your account from email')
+                    return HttpResponseRedirect('/')
+                    
+            else:
+                messages.info(request, 'Enter the correct value!!!')
+        form = TeacherRegForm()
+        return render(request, 'chairman/teacher_signup.html', {'form': form})
+    else:
+        return HttpResponseRedirect('/')
+
+def delete_faculty_page(request):
+    faculty_members = Teacher.objects.all()
+    return render(request, 'chairman/deleteFacultyPage.html', {'faculty_members': faculty_members})
+
+def delete_current_faculty(request, faculty_id):
+    if request.user.is_admin:
+        data = Teacher.objects.get(id= faculty_id)
+        # data.delete()
+        messages.success(request, 'Faculty member deleted successfully')
+        return HttpResponseRedirect('/chairman/delete_faculty_page/')
+    else:
+        return HttpResponseRedirect('/')
+
+def chairman_student_reg(request):
+    if request.user.is_admin:
+        if request.method == 'POST':
+            form = StudentRegForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'A Student added successfully!!!!')
+                return HttpResponseRedirect('/chairman/profile/')
+            else:
+                messages.warning(request, 'Enter the correct data to registration!!!')
+                return HttpResponseRedirect('/chairman')
+        form = StudentRegForm()
+        return render(request, 'chairman/student_signup.html', {'form': form})
+    else:
+        return HttpResponseRedirect('/')
+    
+def delete_student_page(request):
+    if request.user.is_admin:
+        students = Student.objects.all()
+        return render(request, 'chairman/student_list.html', {'students': students})
+    else:
+        return HttpResponseRedirect('/')
+
+def delete_current_student(request, student_id):
+    if request.user.is_admin:
+        data = Student.objects.get(id= student_id)
+        # data.delete()
+        messages.success(request, 'A student deleted successfully')
+        return HttpResponseRedirect('/chairman/delete_student_page/')
+    else:
+        return HttpResponseRedirect('/')
